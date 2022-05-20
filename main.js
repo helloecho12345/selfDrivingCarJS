d7003af583134e725390ed0ea708f53ef79ec344
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-05-20 23:54:03 +0100

    drew car in main.js, imported car in index.html, and created car class in car.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const canvas=document.getElementById("myCanvas");
 canvas.height=window.innerHeight;
 canvas.width=200;
+
+// draw the car on the canvas
+// contains all the things we need
+const ctx = canvas.getContext("2d");
+// 100=x, 100=y, 30 pixel width, 50 pixel height
+const car=new Car(100,100,30,50);
+// car gets drawn using the context
+car.draw(ctx);
+
