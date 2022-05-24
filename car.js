30b36c4d3a802b7c17e0691c1ddb8c9bb7735ec9
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-05-24 08:47:39 +0100

    car object created in car.js and now renders on page

--- car.js
+++ car.js
@@ -1,7 +1,19 @@
 class Car{
-  constructor(x,y,width, height)
+  constructor(x,y,width,height){
     this.x=x;
     this.y=y;
     this.width=width;
     this.height=height;
+}
+// draw is a method that gets context as a parameter
+draw(ctx){
+  ctx.beginPath();
+  ctx.rect(
+    // the x of the car is going to be the center of the car
+    this.x-this.width/2,
+    this.y-this.height/2,
+    this.width,
+    this.height
+  );
+  ctx.fill();
 }
